20abbe1c7bc740cc47fc358fc031a1b8204a58bf
sajjadhossain <sajjadhossain0756@gmail.com>
2025-07-23 20:50:46 +0600

    backend part-1 complete

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,28 @@
 import express, { Request, Response } from 'express';
+import cors from "cors"
+import { router } from './app/routes';
+import { globalErrorHandler } from './app/middlewares/globalErrorHandler';
+import { notFound } from './app/middlewares/notFound';
+
 
 const app = express();
 
-app.get("/",(req: Request,res: Response)=>{
-   res.status(200).json({
-     message: "welcome to Tour Management App Backend"
-   })
+
+app.use(express.json())
+app.use(cors())
+
+app.use("/api/v1", router)
+
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "welcome to Tour Management App Backend"
+  })
 })
+
+// global error handler;
+app.use(globalErrorHandler);
+// not-found route;
+app.use(notFound)
 
 export default app;
